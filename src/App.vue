e3d9a3715eddadeec71a83266b00171b33d49b08
pizzi9523 <pizzitolagiuseppe@live.it>
2021-11-11 16:58:44 +0100

    Complete Main

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <SiteHeader />
+    <SiteMain />
     <!-- <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
@@ -8,17 +9,18 @@
 
 <script>
 import SiteHeader from "./components/SiteHeader.vue";
+import SiteMain from "./components/SiteMain.vue";
 
 export default {
   name: "App",
   components: {
     SiteHeader,
+    SiteMain,
   },
 };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: "Greta Sans Condensed Bold", "Greta Sans Bold", Arial, sans-serif;
-}
+@import "./assets/scss/variables.scss";
+@import "./assets/scss/commons.scss";
 </style>
